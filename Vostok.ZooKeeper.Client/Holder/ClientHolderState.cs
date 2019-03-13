ab43d12ebd0182477c4c74f03cacca39d6442880
kungurtsev <kungurtsev@skbkontur.ru>
2019-03-13 15:10:22 +0500

    extract to file

---
+++ Vostok.ZooKeeper.Client/Holder/ClientHolderState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Vostok.ZooKeeper.Client.Abstractions.Model;
+using ZooKeeperNetExClient = org.apache.zookeeper.ZooKeeper;
+
+namespace Vostok.ZooKeeper.Client.Holder
+{
+    internal class ClientHolderState : IDisposable
+    {
+        public readonly Lazy<ZooKeeperNetExClient> LazyClient;
+        public readonly ConnectionState ConnectionState;
+        public readonly DateTime StateChanged = DateTime.UtcNow;
+        public readonly ConnectionWatcher ConnectionWatcher;
+        public readonly TaskCompletionSource<ClientHolderState> NextState = new TaskCompletionSource<ClientHolderState>();
+
+        public ClientHolderState(Lazy<ZooKeeperNetExClient> client, ConnectionWatcher connectionWatcher, ConnectionState connectionState)
+        {
+            LazyClient = client;
+            ConnectionState = connectionState;
+            ConnectionWatcher = connectionWatcher;
+        }
+
+        public ZooKeeperNetExClient Client => LazyClient?.Value;
+
+        public void Dispose()
+        {
+            Client.Dispose();
+        }
+
+        public override string ToString() =>
+            $"{ConnectionState} at {StateChanged}";
+    }
+}
