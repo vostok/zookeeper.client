72965738137170f0854ba0e16af2723edd717868
Татьяна Гладышева <gladysheva@skbkontur.ru>
2020-10-23 13:37:37 +0500

    Add AuthenticationInfoComparer

---
+++ Vostok.ZooKeeper.Client/Helpers/AuthenticationInfoComparer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Vostok.ZooKeeper.Client.Abstractions.Model.Authentication;
+
+namespace Vostok.ZooKeeper.Client.Helpers
+{
+    internal class AuthenticationInfoComparer : IEqualityComparer<AuthenticationInfo>
+    {
+        public static readonly AuthenticationInfoComparer Instance = new AuthenticationInfoComparer();
+
+        public bool Equals(AuthenticationInfo x, AuthenticationInfo y)
+        {
+            if (ReferenceEquals(x, y))
+                return true;
+
+            if (x == null || y == null)
+                return false;
+
+            return string.Equals(x.Scheme, y.Scheme) && x.Data.SequenceEqual(y.Data);
+        }
+
+        public int GetHashCode(AuthenticationInfo obj)
+        {
+            return (obj.Scheme.GetHashCode() * 397) ^ obj.Data.Aggregate(obj.Data.Length, (current, element) => (current * 397) ^ element.GetHashCode());
+        }
+    }
+}
