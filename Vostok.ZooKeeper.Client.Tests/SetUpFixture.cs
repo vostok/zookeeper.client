5d5096f924a0eb19fa8813fe49543ee78dc726fd
kungurtsev <kungurtsev@skbkontur.ru>
2021-01-21 16:59:12 +0500

    configure tracer in tests

---
+++ Vostok.ZooKeeper.Client.Tests/SetUpFixture.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Vostok.Tracing;
+using Vostok.Tracing.Abstractions;
+
+namespace Vostok.ZooKeeper.Client.Tests
+{
+    [SetUpFixture]
+    internal class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            TracerProvider.Configure(new Tracer(new TracerSettings(new DevNullSpanSender())));
+        }
+    }
+}
