3cae0de75984ffb39d12425af87868b47c997adb
Ilya Tzoop <ilyatzoop@gmail.com>
2022-04-13 14:41:27 +0500

    Escape log messages

---
+++ Vostok.ZooKeeper.Client.Tests/Helpers/VostokMessageTemplateEscaper_Tests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Vostok.ZooKeeper.Client.Helpers;
+
+namespace Vostok.ZooKeeper.Client.Tests.Helpers
+{
+    [TestFixture]
+    public class VostokMessageTemplateEscaper_Tests
+    {
+        [Test]
+        public void Should_EscapeBrackets_CasualUsage()
+        {
+            VostokMessageTemplateEscaper.Escape
+                    ("Session establishment complete on server {10.217.9.47:2181}, sessionid = 0x5047bed84ab9a42, negotiated timeout = 10000")
+               .Should()
+               .Be("Session establishment complete on server {{10.217.9.47:2181}}, sessionid = 0x5047bed84ab9a42, negotiated timeout = 10000");
+        }
+    }
+}
